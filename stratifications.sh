bb51e75751d3be216866263028c696dc29a5c287
ACEnglish <english@spiralgenetics.com>
2018-07-16 18:28:09 +0000

    Better vcf output
    The outputted results are now full vcfs and better handling of
    the repeat annotation bed

--- stratifications.sh
+++ stratifications.sh
@@ -2,8 +2,8 @@
 ## Arg1 = the Truvari result directory to operate over
 ## Arg2 = The repeats annotation file
 
+repeats=`readlink -f $2`
 cd $1
-repeats=$2 #../repeats_trfSimplerepLowcomplex_merged50_slop50_gt100.bed.gz
 # FN/INS/non-TR
 (grep "#" fn.vcf && (grep 'SVTYPE=INS' fn.vcf | grep 'TRgt100=FALSE')) | vcf-sort | bgzip > fn_ins_nonTR.vcf.gz
 tabix -p vcf fn_ins_nonTR.vcf.gz
@@ -13,14 +13,14 @@ tabix -p vcf fn_ins_TR.vcf.gz
 
 # FP/INS/non-TR
 # grep -v "#" fp.vcf | awk '{if (length($4) - length($5) < 0) print $0}' | bedtools intersect -a stdin \
-(grep "#" fp.vcf && grep 'SVTYPE=INS' fp.vcf) | bedtools intersect -a stdin \
-            -b $repeats -f 0.2 -v | vcf-sort | bgzip > fp_ins_nonTR.vcf.gz
+(grep "#" fp.vcf && (grep "#" fp.vcf && grep 'SVTYPE=INS' fp.vcf) | bedtools intersect -a stdin \
+            -b $repeats -f 0.2 -v) | vcf-sort | bgzip > fp_ins_nonTR.vcf.gz
 tabix -p vcf fp_ins_nonTR.vcf.gz
 
 # FP/INS/TR
 # grep -v "#" fp.vcf | awk '{if (length($4) - length($5) < 0) print $0}' | bedtools intersect -a stdin \
-(grep "#" fp.vcf && grep 'SVTYPE=INS' fp.vcf) | bedtools intersect -a stdin \
-            -b $repeats -f 0.2 -u | vcf-sort | bgzip > fp_ins_TR.vcf.gz
+(grep "#" fp.vcf && (grep "#" fp.vcf && grep 'SVTYPE=INS' fp.vcf) | bedtools intersect -a stdin \
+            -b $repeats -f 0.2 -u) | vcf-sort | bgzip > fp_ins_TR.vcf.gz
 tabix -p vcf fp_ins_TR.vcf.gz
 
 # FN/DEL/non-TR
@@ -31,12 +31,12 @@ tabix -p vcf fn_del_nonTR.vcf.gz
 tabix -p vcf fn_del_TR.vcf.gz
 # FP/DEL/non-TR
 # grep -v "#" fp.vcf | awk '{if (length($4) - length($5) > 0) print $0}' | bedtools intersect -a stdin \
-(grep "#" fp.vcf && grep 'SVTYPE=DEL' fp.vcf) | bedtools intersect -a stdin \
-            -b $repeats -f 0.2 -v | vcf-sort | bgzip > fp_del_nonTR.vcf.gz
+(grep "#" fp.vcf && (grep "#" fp.vcf && grep 'SVTYPE=DEL' fp.vcf) | bedtools intersect -a stdin \
+            -b $repeats -f 0.2 -v) | vcf-sort | bgzip > fp_del_nonTR.vcf.gz
 tabix -p vcf fp_del_nonTR.vcf.gz
 # FP/DEL/TR
 # grep -v "#" fp.vcf | awk '{if (length($4) - length($5) > 0) print $0}' | bedtools intersect -a stdin \
-(grep "#" fp.vcf && grep 'SVTYPE=DEL' fp.vcf) | bedtools intersect -a stdin \
-            -b $repeats -f 0.2 -u | vcf-sort | bgzip > fp_del_TR.vcf.gz
+(grep "#" fp.vcf && (grep "#" fp.vcf && grep 'SVTYPE=DEL' fp.vcf) | bedtools intersect -a stdin \
+            -b $repeats -f 0.2 -u) | vcf-sort | bgzip > fp_del_TR.vcf.gz
 tabix -p vcf fp_del_TR.vcf.gz
 cd - 
