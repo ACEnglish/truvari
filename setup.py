c05f821795786cf5debaeab765dca62de2453a5d
ACEnglish <english@spiralgenetics.com>
2019-06-05 22:11:55 +0000

    making consistency_report executable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Truvari',
-    version='1.0',
+    version='1.1',
     author="ACEnglish",
     author_email="acenglish@gmail.com",
     url="https://github.com/spiralgenetics/truvari",
