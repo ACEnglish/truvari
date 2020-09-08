c367043b155fb17efd84dc600c4f361dbd6f4c05
Adam English <englis@spiralgenetics.com>
2020-09-07 20:01:45 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Truvari',
-    version='2.0.2-dev',
+    version='2.0.2',
     author="ACEnglish",
     author_email="acenglish@gmail.com",
     url="https://github.com/spiralgenetics/truvari",
