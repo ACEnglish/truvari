5a607502669cf45a272de4c2ca6e34113db4848a
Adam English <ACEnglish@gmail.com>
2021-08-06 01:38:57 -0400

    autobadge work

--- repo_utils/pylint_maker.py
+++ repo_utils/pylint_maker.py
@@ -1,19 +1,24 @@
 """
 Utility to run pylint and update the badge
 """
+import re
+import sys
+from io import StringIO
 import anybadge
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
-from io import StringIO
 
 fail_under = 9.33
+
+# run pylint
 output = StringIO()
 Run(['--rcfile=.pylintrc', f'--fail-under={fail_under}', 'truvari'], reporter=TextReporter(output), exit=False)
 output.seek(0)
+output = output.read()
 
 # Get the score
-pylint_score = float(output.read().strip().split('\n')[-1].split(' ')[-1].split('/')[0])
-
+search = re.search("Your code has been rated at (?P<score>.*)/10", output)
+pylint_score = search.groupdict()['score']
 
 # Define thresholds: <2=red, <4=orange <8=yellow <10=green
 thresholds = {2: 'red',
@@ -21,7 +26,9 @@ thresholds = {2: 'red',
               6: 'yellow',
               fail_under: 'green'}
 
-badge = anybadge.Badge('pylint', output, thresholds=thresholds)
+badge = anybadge.Badge('pylint', pylint_score, thresholds=thresholds)
 
-badge.write_badge('imgs/pylint.svg')
+badge.write_badge('imgs/pylint.svg', overwrite=True)
+
+sys.stdout.write(output)
 
