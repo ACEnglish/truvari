06f0369e7fc83259f8720b2a18699e1302c64a05
Adam English <ACEnglish@gmail.com>
2021-08-17 00:25:21 -0500

    grm tests

--- repo_utils/truvari_ssshtests.sh
+++ repo_utils/truvari_ssshtests.sh
@@ -7,20 +7,17 @@ BASDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INDIR=$BASDIR/test_files
 ANSDIR=$INDIR/answer_key/
 
+# ------------------------------------------------------------
+#                                 test helpers
+# ------------------------------------------------------------
+
 fn_md5() {
+    # simple md5sum checking
     md5sum $1 | cut -f1 -d\  
 }
 
-# ------------------------------------------------------------
-#                                 version
-# ------------------------------------------------------------
-run test_version truvari version
-assert_in_stdout "$(python -c 'import truvari; print(f"Truvari v{truvari.__version__}")')"
-
-# ------------------------------------------------------------
-#                                 bench
-# ------------------------------------------------------------
 bench() {
+    # run and test truvari bench
     f1=$1
     f2=$2
     k=${f1}${f2}
@@ -39,15 +36,8 @@ bench() {
     done
 }
 
-bench 1 2
-bench 1 3
-bench 2 3
-
-# ------------------------------------------------------------
-#                                 collapse
-# ------------------------------------------------------------
-
 collapse_hap() {
+    # run and test truvari collapse
     run test_collapse_$1 truvari collapse -f $INDIR/reference.fa \
                      -i $INDIR/input${1}.vcf.gz \
                      -o input${1}_collapsed.vcf \
@@ -63,6 +53,49 @@ collapse_hap() {
     assert_equal $(fn_md5 $ANSDIR/input${1}_removed.vcf) $(fn_md5 input${1}_removed.vcf)
 }
 
+info_tests() {
+    # pull and query info fields from vcfs
+    test $(which bcftools) || sudo apt-get install bcftools 
+    name=$1
+    base_vcf=$ANSDIR/anno_answers.vcf.gz
+    comp_vcf=$2
+    infos=$3
+    bcftools query -f "%CHROM\t%POS\t${INFOS}\n" $base_vcf | sort > answer.txt
+    bcftools query -f "%CHROM\t%POS\t${INFOS}\n" $comp_vcf | sort > result.txt
+    run test_infos_${name}
+    assert_equal $(fn_md5 answer.txt) $(fn_md5 result.txt)
+}
+
+df_check() {
+    # check if joblib saved pandas dataframes are equivalent
+    test_name=$1
+    base_df=$2
+    comp_df=$3
+    run $test_name python -c """
+import joblib;
+a = joblib.load(\"$base_df\")
+b = joblib.load(\"$comp_df\")
+assert a.equals(b);
+"""
+    assert_exit_code $? 0
+}
+
+# ------------------------------------------------------------
+#                                 version
+# ------------------------------------------------------------
+run test_version truvari version
+assert_in_stdout "$(python -c 'import truvari; print(f"Truvari v{truvari.__version__}")')"
+
+# ------------------------------------------------------------
+#                                 bench
+# ------------------------------------------------------------
+bench 1 2
+bench 1 3
+bench 2 3
+
+# ------------------------------------------------------------
+#                                 collapse
+# ------------------------------------------------------------
 collapse_hap 1
 collapse_hap 2
 collapse_hap 3
@@ -82,7 +115,6 @@ assert_equal $(fn_md5 $ANSDIR/multi_removed.vcf) $(fn_md5 multi_removed.vcf)
 # ------------------------------------------------------------
 #                                 consistency
 # ------------------------------------------------------------
-
 cp $INDIR/input*.vcf.gz ./
 run test_consistency truvari consistency input*.vcf.gz
 assert_exit_code $? 0
@@ -94,20 +126,9 @@ assert_equal $(fn_md5 $ANSDIR/consistency.txt) $(fn_md5 $STDOUT_FILE)
 # ------------------------------------------------------------
 #                                 anno
 # ------------------------------------------------------------
-test $(which bcftools) || sudo apt-get install bcftools 
-info_tests() {
-    name=$1
-    base_vcf=$ANSDIR/anno_answers.vcf.gz
-    comp_vcf=$2
-    infos=$3
-    bcftools query -f "%CHROM\t%POS\t${INFOS}\n" $base_vcf | sort > answer.txt
-    bcftools query -f "%CHROM\t%POS\t${INFOS}\n" $comp_vcf | sort > result.txt
-    run test_infos_${name}
-    assert_equal $(fn_md5 answer.txt) $(fn_md5 result.txt)
-}
-
 VCF=$INDIR/multi.vcf.gz
 REF=$INDIR/reference.fa
+
 #                                 hompct
 run test_anno_hompct truvari anno hompct -i $VCF -o anno_hompct.vcf
 assert_exit_code $? 0
@@ -134,10 +155,12 @@ assert_exit_code $? 0
 info_tests numneigh anno_numneigh.vcf INFO/NumNeighbors,INFO/NeighId
 
 #                                 grm
+run test_anno_grm truvari anno grm -i $INDIR/input2.vcf.gz -r $REF -o grm.jl
+assert_exit_code $? 0
 
-#These can also be tested with the rest of the anno framework. 
-#However I need extra tools to make it happen.
+df_check test_grm_result $ANSDIR/grm.jl grm.jl
 
+# requires external executables
 #                                 repmask
 #                                 trf
 
@@ -147,10 +170,4 @@ info_tests numneigh anno_numneigh.vcf INFO/NumNeighbors,INFO/NeighId
 run test_vcf2df truvari vcf2df -f -i $INDIR/input1.vcf.gz vcf2df.jl
 assert_exit_code $? 0
 
-run test_vcf2df_result python -c """
-import joblib;
-a = joblib.load(\"$ANSDIR/vcf2df.jl\")
-b = joblib.load(\"vcf2df.jl\")
-assert a.equals(b);
-"""
-assert_exit_code $? 0
+df_check test_vcf2df_result $ANSDIR/vcf2df.jl vcf2df.jl
