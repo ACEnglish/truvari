c9da44af5772fc1369f3df0299b45554ed1f552a
Adam English <englis@spiralgenetics.com>
2021-08-19 18:03:01 -0700

    Adding chm13 simple repeats resource

--- resources/all_fields2truvbed.py
+++ resources/all_fields2truvbed.py
@@ -1,8 +1,8 @@
-#print("""##INFO=<ID=SREP_periods,Number=.,Type=Float,Description="Simple Repeat period lengths">
-##INFO=<ID=SREP_copies,Number=.,Type=Float,Description="Simple Repeat copy numbers">
-##INFO=<ID=SREP_scores,Number=.,Type=Integer,Description="Simple Repeat scores">
-##INFO=<ID=SREP_entropies,Number=.,Type=Float,Description="Simple Repeat entropies">
-##INFO=<ID=SREP_repeats,Number=.,Type=String,Description="Simple Repeat sequences">""")
+print("""##INFO=<ID=SREP_periods,Number=.,Type=Float,Description="Simple Repeat period lengths">
+#INFO=<ID=SREP_copies,Number=.,Type=Float,Description="Simple Repeat copy numbers">
+#INFO=<ID=SREP_scores,Number=.,Type=Integer,Description="Simple Repeat scores">
+#INFO=<ID=SREP_entropies,Number=.,Type=Float,Description="Simple Repeat entropies">
+#INFO=<ID=SREP_repeats,Number=.,Type=String,Description="Simple Repeat sequences">""")
 
 # Given an all_fields output from UCSC genome browser simple repeats, make a anno trf bed
 import sys
