b4b84da9d557b394c3445df5ea0adf0bbd14236a
Adam English <ACEnglish@gmail.com>
2021-09-21 18:46:34 -0700

    Update conf.py

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -9,9 +9,6 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import subprocess
-subprocess.run(['python3','setup.py'], cwd="../../")
 
 
 # -- Project information -----------------------------------------------------
