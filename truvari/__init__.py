abe3cbc18244cfc45b359ed373e2dd91ee3bfaaa
Adam English <ACEnglish@gmail.com>
2021-10-03 14:19:05 -0700

    Reverting to 2aa3d14

--- truvari/__init__.py
+++ truvari/__init__.py
@@ -1,63 +1,121 @@
 """
-The ``truvari` library provides methods for comparing vcf entries and calculating performance metrics for discovery
-performance
+Full documentation at https://github.com/spiralgenetics/truvari/wiki
 
-User Manual:
-    https://github.com/spiralgenetics/truvari
+See `help()` of specific functions / objects for details
+
+methods:
+:meth:`allele_freq_annos`
+:meth:`bed_ranges`
+:meth:`cmd_exe`
+:meth:`copy_entry`
+:meth:`create_pos_haplotype`
+:meth:`entry_boundaries`
+:meth:`entry_create_haplotype`
+:meth:`entry_gt_comp`
+:meth:`entry_is_present`
+:meth:`entry_pctsim`
+:meth:`entry_reciprocal_overlap`
+:meth:`entry_same_variant_type`
+:meth:`entry_size`
+:meth:`entry_size_similarity`
+:meth:`entry_to_key`
+:meth:`entry_variant_type`
+:meth:`fetch_coords`
+:meth:`filter_value`
+:meth:`get_gt`
+:meth:`get_scalebin`
+:meth:`get_sizebin`
+:meth:`get_svtype`
+:meth:`help_unknown_cmd`
+:meth:`make_interval_tree`
+:meth:`match_sorter`
+:meth:`optimize_df_memory`
+:meth:`overlaps`
+:meth:`reciprocal_overlap`
+:meth:`ref_ranges`
+:meth:`restricted_float`
+:meth:`setup_logging`
+:meth:`setup_progressbar`
+:meth:`sizesim`
+:meth:`vcf_to_df`
+:meth:`weighted_score`
+
+objects:
+:class:`GT`
+:class:`GenomeTree`
+:class:`LogFileStderr`
+:class:`SV`
+
+data:
+:data:`truvari.HEADERMAT`
+:data:`truvari.MATCHRESULT`
+:data:`truvari.QUALBINS`
+:data:`truvari.SVTYTYPE`
+:data:`truvari.SZBINMAX`
+:data:`truvari.SZBINS`
+:data:`truvari.SZBINTYPE`
 """
-__version__ = '3.0.1'
+
+__version__ = '3.1.0-dev'
+
+from truvari.vcf2df import (
+    GT,
+    QUALBINS,
+    SV,
+    SVTYTYPE,
+    SZBINMAX,
+    SZBINS,
+    SZBINTYPE,
+    get_gt,
+    get_scalebin,
+    get_sizebin,
+    get_svtype,
+    optimize_df_memory,
+    vcf_to_df,
+)
+
+from truvari.annos.af_calc import (
+    allele_freq_annos
+)
+
+from truvari.genome_tree import (
+    GenomeTree,
+    make_interval_tree
+)
+
+
 from truvari.utils import (
-    StatsBox,
-    setup_progressbar,
+    HEADERMAT,
     LogFileStderr,
+    MATCHRESULT,
+    bed_ranges,
+    cmd_exe,
+    copy_entry,
+    help_unknown_cmd,
+    ref_ranges,
+    restricted_float,
     setup_logging,
-    cmd_exe
+    setup_progressbar,
 )
 
 from truvari.comparisons import (
-    entry_is_variant,
-    entry_to_key,
-    sizesim,
-    entry_size_similarity,
-    entry_gt_comp,
-    create_haplotype,
     create_pos_haplotype,
-    entry_pctsim,
-    overlaps,
-    entry_variant_type,
-    same_variant_type,
-    fetch_coords,
     entry_boundaries,
-    entry_size,
-    weighted_score,
-    reciprocal_overlap,
+    entry_create_haplotype,
+    entry_gt_comp,
+    entry_is_present,
+    entry_pctsim,
     entry_reciprocal_overlap,
-    is_sv,
+    entry_same_variant_type,
+    entry_size,
+    entry_size_similarity,
+    entry_to_key,
+    entry_variant_type,
+    fetch_coords,
     filter_value,
     match_sorter,
-    copy_entry
-)
-
-from truvari.genome_tree import *
-
-from truvari.giab_report import make_giabreport
-
-from truvari.stats import (
-    SZBINS,
-    SZBINMAX,
-    QUALBINS,
-    GT,
-    SV,
-    get_svtype,
-    get_sizebin,
-    get_gt,
-    get_scalebin
-)
-
-from truvari.annos.af_calc import allele_freq_annos
-
-from truvari.vcf2df import (
-    vcf_to_df,
-    SZBINTYPE,
-    SVTYTYPE
+    overlaps,
+    reciprocal_overlap,
+    sizesim,
+    weighted_score,
 )
