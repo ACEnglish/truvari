1a9bf9d30c1a4623b54be758f8a3aabc7dce767c
Adam English <ACEnglish@gmail.com>
2023-05-24 08:26:22 -0400

    attempting cicd test

--- repo_utils/sub_tests/phab.sh
+++ repo_utils/sub_tests/phab.sh
@@ -12,16 +12,15 @@ if [ $test_phab ]; then
     assert_equal $(fn_md5 $ANSDIR/phab/phab_result.vcf.gz) $(fn_md5 $OD/phab_result.vcf.gz)
 fi
 
-# can't test until I figure out how to install pywfa
-#run test_phab_wfa $truv phab -o $OD/phab_result_wfa.vcf.gz \
-                         #-b $INDIR/variants/phab_base.vcf.gz \
-                         #-c $INDIR/variants/phab_comp.vcf.gz \
-                         #-f $INDIR/references/phab_ref.fa \
-                         #-r chr1:700-900 --align wfa
-#if [ $test_phab_wfa ]; then
-    #assert_exit_code 0
-    #assert_equal $(fn_md5 $ANSDIR/phab/phab_result_wfa.vcf.gz) $(fn_md5 $OD/phab_result_wfa.vcf.gz)
-#fi
+run test_phab_wfa $truv phab -o $OD/phab_result_wfa.vcf.gz \
+                         -b $INDIR/variants/phab_base.vcf.gz \
+                         -c $INDIR/variants/phab_comp.vcf.gz \
+                         -f $INDIR/references/phab_ref.fa \
+                         -r chr1:700-900 --align wfa
+if [ $test_phab_wfa ]; then
+    assert_exit_code 0
+    assert_equal $(fn_md5 $ANSDIR/phab/phab_result_wfa.vcf.gz) $(fn_md5 $OD/phab_result_wfa.vcf.gz)
+fi
 
 run test_phab_badparams $truv phab -o $OD/phab_result\
     -b doesntexist.vcf \
