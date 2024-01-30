11c56e2da522dc06d30f8eae15b5c5ca0fc0ec23
Adam English <ACEnglish@gmail.com>
2024-01-30 17:51:02 -0500

    matching clean
    prettier syntax by leveraging match on tuples
    Also added calc_score and forgotten seqsim attribute

--- trust/src/matching.rs
+++ trust/src/matching.rs
@@ -12,6 +12,7 @@ pub struct MatchResult {
     pub comp_gt_count: u8,
     pub state: bool,
     pub seqsim: Option<f32>,
+    pub sizesim: Option<f32>,
     pub ovlpct: Option<f32>,
     pub sizediff: Option<i64>,
     pub st_dist: Option<i64>,
@@ -33,6 +34,7 @@ impl MatchResult {
             comp_gt_count: 0,
             state: false,
             seqsim: None,
+            sizesim: None,
             ovlpct: None,
             sizediff: None,
             st_dist: None,
@@ -41,6 +43,13 @@ impl MatchResult {
             multi: None,
             score: None,
             matid: None,
+        }
+    }
+
+    pub fn calc_score(mut self) {
+        self.score = match (self.seqsim, self.sizesim, self.ovlpct) {
+            (Some(seq), Some(size), Some(ovl)) => Some((seq + size + ovl) / 3.0 * 100.0),
+            _ => None,
         }
     }
 }
@@ -60,22 +69,17 @@ impl Ord for MatchResult {
                 Ordering::Less
             }
         } else {
-            match self.score {
-                Some(sscore) => {
-                    match other.score {
-                        Some(oscore) => {
-                            if sscore < oscore {
-                                Ordering::Greater
-                            } else if sscore > oscore {
-                                Ordering::Less
-                            } else {
-                                Ordering::Equal
-                            }
-                        }
-                        None => Ordering::Equal, // Not good?
+            match (self.score, other.score) {
+                (Some(sscore), Some(oscore)) => {
+                    if sscore < oscore {
+                        Ordering::Greater
+                    } else if sscore > oscore {
+                        Ordering::Less
+                    } else {
+                        Ordering::Equal
                     }
                 }
-                None => Ordering::Equal,
+                _ => Ordering::Equal, // Not good?
             }
         }
     }
@@ -90,10 +94,11 @@ impl PartialEq for MatchResult {
 impl Eq for MatchResult {}
 
 /* TODO
-class MatchResult():  # pylint: disable=too-many-instance-attributes
-    calc_score
-    Ord
+pub struct Matcher {
+}
 class Matcher():
+    make_match_params with defaults...
+    make_match_params_from_args with the thing
     def filter_call(self, entry, base=False):
     def build_match(self, base, comp, matid=None, skip_gt=False, short_circuit=False):
 def file_zipper(*start_files):
