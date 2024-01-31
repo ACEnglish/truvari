b6ac599562edb5fa0e1c899784d5b4a283936023
Adam English <ACEnglish@gmail.com>
2024-01-31 00:43:20 -0500

    collapse fix

--- truvari/collapse.py
+++ truvari/collapse.py
@@ -154,7 +154,7 @@ def collapse_chunk(chunk, matcher):
             m_collap.matches = [mats.pop(0)]
 
         # Remove everything that was used
-        remaining_calls = [_ for _ in remaining_calls if _ not in m_collap.matches]
+        remaining_calls = [_.comp for _ in remaining_calls if _.comp not in m_collap.matches]
 
     if matcher.no_consolidate:
         for val in ret:
